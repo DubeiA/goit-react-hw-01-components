11dfa1fb06d514b12d57a7227cd2dc48d7b88736
Dubei Anatolii <105717456+DubeiA@users.noreply.github.com>
2022-12-11 13:14:31 +0200

    Done 2 task

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,11 +1,32 @@
 import user from "../user.json"
+import data from "./Statistics/data.json";
 import { Profiles } from "./Profiles/Profiles"
+import { Statistics } from "./Statistics/Statistics";
 
+
+
+console.log(data);
 export const App = () => {
   return (
     
-    <Profiles avatar={user.avatar} tag={user.tag} username={user.username}
+    <div style={{
+      display: "flex",
+      justifyContent: "center",
+      alignItems: "end",
+
+      gap: "15px"
+      
+        // padding: "12px 16px",
+        
+        
+      }}>
+      <Profiles avatar={user.avatar} tag={user.tag} username={user.username}
       location={user.location} followers={user.stats.followers } likes={user.stats.likes } views={user.stats.views }/>
     
+    
+    <Statistics title="Upload stats" stats={data} />
+      <Statistics stats={data} />
+    
+    </div>
   );
 };
